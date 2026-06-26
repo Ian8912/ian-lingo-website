921e2cb1a72c318260f578132c5bdb2397b14d1d
Ian Lingo <lingo.ian@yahoo.com>
2026-06-26 11:05:25 -0700

    feat: update Navbar component to indicate current page URL path

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,30 +1,50 @@
-import React from 'react'
+// Tells Next.js that this component must run in the browser. Is a necessity since usePathname() is a client-side hook.
+"use client"
+
+// Link is a Next.js component for navigating between pages without doing a full page reload
 import Link from 'next/link'
+// usePathname() gets the current URL path
+import { usePathname } from 'next/navigation';
+
+// An array of objects representing one navbar link. This allows storing the data and looping over it later. 
+const navLinks = [
+    { name: "Home" , href: "/"},
+    { name: "Projects" , href: "/projects"},
+    { name: "Experience" , href: "/experience"},
+    { name: "Resume" , href: "/resume"},
+    { name: "Contact" , href: "/contact"},
+];
 
 export default function Navbar() {
-  return (
-    <nav className="fixed top-0 w-full flex items-center justify-around py-5 px-24 border-b border-gray-700 bg-black">
-        <ul className="flex gap-10 text-lg">
-            <Link href="/" className="text-gray-300 hover:text-sky-400 transition-colors">
-                Home
-            </Link>
+    // pathname stores the current page URL path
+    const pathname = usePathname();
 
-            <Link href="/projects" className="text-gray-300 hover:text-sky-400 transition-colors">
-                Projects
-            </Link>
+    return (
+        <nav className="fixed top-0 z-50 w-full flex items-center justify-around py-5 px-24 border-b border-gray-700 bg-black">
+            <ul className="flex gap-10 text-2xl">
+                {navLinks.map((link) => {       // Loops through every item in the navLinks array.
+                    const isActive =        // Checks where the current link should be highlighter (if it's active).
+                        link.href === "/"
+                        ? pathname === "/"
+                        : pathname.startsWith(link.href);       // All subdirectories within navLinks item will be active (e.g. /projects/mars, /projects, /projects/chess).
 
-            <Link href="/experience" className="text-gray-300 hover:text-sky-400 transition-colors">
-                Experience
-            </Link>
-
-            <Link href="/resume" className="text-gray-300 hover:text-sky-400 transition-colors">
-                Resume
-            </Link>
-
-            <Link href="/contact" className="text-gray-300 hover:text-sky-400 transition-colors">
-                Contact
-            </Link>
-        </ul>
-    </nav>
-  );
+                    return (
+                        // Each generated item needs a unique key, so React can track it efficiently. link.href works because each URL is unique.
+                        <li key={link.href}>
+                            <Link
+                                href={link.href}
+                                className={`transition-colors ${        // Ternary operator checks if URL path is active, if so then text sky 400 color and semibold, else then keep gray.
+                                isActive
+                                    ? "text-sky-400 font-semibold"
+                                    : "text-gray-400 hover:text-sky-400"
+                                }`}
+                            >
+                                {link.name}
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
 }
