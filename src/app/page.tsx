558f55800804ba3cc3793643303ded731ee10423
Ian Lingo <lingo.ian@yahoo.com>
2026-06-26 11:04:11 -0700

    feat: update Home page with image and paragraphs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="space-y-3 max-w-3xl">
-        <h1 className="text-5xl fond-semibold">Home Page</h1>
-        <p className="text-gray-400">Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development.</p>
-      </div>
+    <div className="flex flex-col items-center text-center">
+      <Image
+        src="/images/ian-lingo.png"
+        alt="Ian Lingo Headshot"
+        width={400}
+        height={80}
+        className="mt-30 rounded-xl object-cover"
+        priority
+      />
+        <h2 className="mt-4 text-2xl font-bold">Ian Lingo</h2>
+        <h3 className="mt-4 text-xl">Software Engineer</h3>
+        <h1 className="mt-12 text-4xl font-bold">About Me</h1>
+        <p className="mt-4 max-w-lg text-lg text-gray-400">Hello (World) ! My name is Ian Lingo, and I am an aspiring software engineer based in Austin, Texas. I graduated with a Bachelor&apos;s of Science in Computer Science with minors in Applied Math and Mass Communication from Texas State University.</p>
+        <p className="mt-4 max-w-lg text-lg text-gray-400">I am highly interested in all things technology and business related. Beyond coding, I thoroughly enjoy traveling, reading, and going to the gym. I&apos;m open to trying new things as I would for learning new tools and technologies most suitable for the job.</p>
     </div>
   );
 }
